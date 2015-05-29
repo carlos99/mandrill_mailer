da7f7c472bc64821219e7d55fa9db1b968ecf864
carlos99 <carlos.cw2106@gmail.com>
2015-05-28 22:44:58 -0600

    The controller that allows overwrite the defauls devise methods has been created. Now the user can sign_up and sign_up. The projects is linked to the corresponding user. The project model has been created. Some basic styles and navigation partial has been added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  root 'home#index'
+
+  resources :projects
+
   devise_for :users, controllers: {registrations: "registrations"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
